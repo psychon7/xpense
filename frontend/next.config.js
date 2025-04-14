947bd7814ca098b1f83c22e917f537982ce6a779
Mohan <47470954+psychon7@users.noreply.github.com>
2025-04-14 16:25:16 +0800

    chore: Updated Next.js config for static export

---
+++ frontend/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
